ae20b25e8293ee00af251540f33e35486d86b53c
Naomi Esparza <up230480@alumnos.upa.edu.mx>
2026-02-20 21:51:28 -0600

    Tarea 5

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,62 @@
 module Helper exposing (..)
+
+
+-- 1️
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map
+        (\grade ->
+            if grade < 0 then
+                Pending
+
+            else if grade >= 7 then
+                Approved
+
+            else
+                Failed
+        )
+        grades
+
+
+
+-- 2️
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+
+-- 2.2
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+
+-- 2.3
+
+airportAction : List AirplaneStatus -> List String
+airportAction statusList =
+    List.map airplaneScheduleAction statusList
